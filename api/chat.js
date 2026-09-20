178c6459df627ccef0e8446d79843c407e7f3c26
aiongenesisss-commits <aion.genesisss@gmail.com>
2026-09-20 10:42:10 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,5 @@
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
   
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ success: false, error: 'Method not allowed' });
   }
   
   const message = req.body.message;
@@ -65,4 +65,4 @@ export default async function handler(req, res) {
       error: 'Server error: ' + error.message 
     });
   }
-}
+};
