15a9a811db4ebe7be0b2a5a2390c33ecc25a0d22
aiongenesisss-commits <aion.genesisss@gmail.com>
2026-09-21 16:27:29 +0300

    Create client-id.js

---
+++ api/paypal/client-id.js
@@ -0,0 +1,5 @@
+
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  return res.status(200).json({ clientId: process.env.PAYPAL_CLIENT_ID || '' });
+}
